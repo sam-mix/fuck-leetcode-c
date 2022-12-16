e99891dfc1fa695ef16c2972893387d328aa1817
Sam mix <1045262335@qq.com>
2022-12-16 13:37:02 +0800

    add 00112

--- lib/00112.go
+++ lib/00112.go
@@ -8,12 +8,23 @@ package lib
  *     Right *TreeNode
  * }
  */
-func hasPathSum(root *TreeNode, targetSum int) bool {
+func hasPathSum1(root *TreeNode, targetSum int) bool {
 	if root == nil {
 		return false
 	}
 	if root.Val == targetSum && root.Right == nil && root.Left == nil {
 		return true
+	}
+	newTargetSum := targetSum - root.Val
+	return hasPathSum(root.Left, newTargetSum) || hasPathSum(root.Right, newTargetSum)
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Right == nil && root.Left == nil {
+		return root.Val == targetSum
 	}
 	newTargetSum := targetSum - root.Val
 	return hasPathSum(root.Left, newTargetSum) || hasPathSum(root.Right, newTargetSum)
