152599d881a3ee205b3b084fb91b039b426614e4
Sam mix <1045262335@qq.com>
2022-12-16 14:25:22 +0800

    add 00118

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.19
 
 require layeh.com/gopher-json v0.0.0-20201124131017-552bb3c4c3bf
 
-require github.com/yuin/gopher-lua v0.0.0-20221210110428-332342483e3f // indirect
+require github.com/yuin/gopher-lua v0.0.0-20221210110428-332342483e3f
--- lib/00118.go
+++ lib/00118.go
@@ -1,8 +1,10 @@
 package lib
 
-func generate(n int) (ans [][]int) {
-	for i := 0; i < n; n++ {
-		ansi := []int{1}
+func generate(n int) [][]int {
+	ans := make([][]int, n)
+	for i := 0; i < n; i++ {
+		ansi := make([]int, i+1)
+		ansi[0] = 1
 		for j := 1; j < i; j++ {
 			ansi[j] = ans[i-1][j-1] + ans[i-1][j]
 		}
@@ -11,5 +13,5 @@ func generate(n int) (ans [][]int) {
 		}
 		ans[i] = ansi
 	}
-	return
+	return ans
 }
