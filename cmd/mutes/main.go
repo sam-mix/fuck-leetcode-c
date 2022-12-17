949250ae5e20a67352f182e8e349759cefbfcb38
Sam mix <1045262335@qq.com>
2022-12-17 17:47:29 +0800

    add something

---
+++ cmd/mutes/main.go
@@ -0,0 +1,68 @@
+package main
+
+import "sync"
+
+func scoreboardManager(in <-chan func(map[string]int), done <-chan struct{}) {
+	scoreboard := map[string]int{}
+	for {
+		select {
+		case <-done:
+			return
+		case f := <-in:
+			f(scoreboard)
+		}
+	}
+}
+
+type ChannelScoreboardManager chan func(map[string]int)
+
+func NewChannelScoreboardManager() (ChannelScoreboardManager, func()) {
+	ch := make(ChannelScoreboardManager)
+	done := make(chan struct{})
+	go scoreboardManager(ch, done)
+	return ch, func() {
+		close(done)
+	}
+}
+
+func (csm ChannelScoreboardManager) Update(name string, val int) {
+	csm <- func(m map[string]int) {
+		m[name] = val
+	}
+}
+
+func (csm ChannelScoreboardManager) Read(name string) (int, bool) {
+	var out int
+	var ok bool
+	done := make(chan struct{})
+	csm <- func(m map[string]int) {
+		out, ok = m[name]
+		close(done)
+	}
+	<-done
+	return out, ok
+}
+
+type MutexScoreboardManager struct {
+	l          sync.RWMutex
+	scoreboard map[string]int
+}
+
+func NewMutexScoreboardManager() *MutexScoreboardManager {
+	return &MutexScoreboardManager{
+		scoreboard: map[string]int{},
+	}
+}
+
+func (msm *MutexScoreboardManager) Update(name string, val int) {
+	msm.l.Lock()
+	defer msm.l.Unlock()
+	msm.scoreboard[name] = val
+}
+
+func (msm *MutexScoreboardManager) Read(name string) (int, bool) {
+	msm.l.RLock()
+	defer msm.l.RUnlock()
+	val, ok := msm.scoreboard[name]
+	return val, ok
+}
