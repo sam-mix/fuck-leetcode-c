8e356bc5a9046ab930fa465a5937fe1e3f1e0369
Sam mix <1045262335@qq.com>
2022-12-22 16:53:49 +0800

    add something

---
+++ unit_low_level_io.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+// /*将输入复制到输出*/
+// int main(int argc, char const *argv[])
+// {
+//     char buf[BUFSIZ];
+//     int n;
+
+//     while ((n = read(0, buf, BUFSIZ)) > 0)
+//     {
+//         write(1, buf, n);
+//     }
+//     return 0;
+// }
