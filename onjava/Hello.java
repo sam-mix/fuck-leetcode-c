0dee086883900c9a0524b2e7626fd52f2be4351b
Sam mix <1045262335@qq.com>
2023-01-03 23:04:50 +0800

    add something

---
+++ onjava/Hello.java
@@ -0,0 +1,10 @@
+package onjava;
+
+import java.util.Date;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, it is:");
+        System.out.println(new Date());
+    }
+}
